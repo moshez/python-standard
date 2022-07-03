28770c9460955e8c7ff9242acc0e8606942808d8
Moshe Zadka <moshez@zadka.club>
2022-07-03 17:18:29 +0000

    +docs

--- doc/conf.py
+++
@@ -1,8 +0,0 @@
-extensions = []
-master_doc = 'index'
-project = 'Seasonable'
-copyright = 'Copyright (c) 2021, Moshe Zadka'
-author = 'Moshe Zadka'
-version = ''
-
-exclude_patterns = ['.ipynb_checkpoints/**']
